8e1a27177c75d11033e59279226a84ec6509abbc
joaogato1048-creator <joaogato1048@gmail.com>
2025-10-20 03:05:36 +0000

    atualizado

---
+++ joao.py
@@ -0,0 +1,26 @@
+import pip
+import time
+
+##[s = 0
+#for c in range(0, 4):
+ # n = int(input('Digite um valor: '))
+  ## s += n
+#print('SOMATORIO É IGUAL {}'.format(s)) ]
+
+
+#for c in range(10, 0, -1):
+ #   print(c)
+  #  time.sleep()
+
+
+
+
+#S = 0;
+
+#for c in range(1, 500):
+#   if c % 3 ==0 and c % 2 != 0:
+#        print(c)
+#        S += c
+#print('SEU RESUlTADO FOI [{}]'.format(S))
+
+
