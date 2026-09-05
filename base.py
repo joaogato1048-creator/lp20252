250609951d9764e070ab27383b1ad8d3f65021cb
joaogato1048-creator <joaogato1048@gmail.com>
2026-09-05 01:35:59 +0000

    base py

---
+++ base.py
@@ -0,0 +1,25 @@
+
+def imprimir():
+ x = Hello Word 
+ print(x);
+
+
+def calculoCir():
+    pi = 3.14159;
+    raio= float( input('digite o raio '));
+    area = total 
+
+    print(total= pi * (raio * raio));
+    print(f'area');
+
+
+def mediaaluno():
+    A = int(input('Digite uma nota :  '));
+    B = int(input('Digite outra nota :  '));
+    C = int(input('Digite mais uma  nota :  '));
+    media = 0
+
+    if((A >= 0 and A <= 10) and (B >= 0 and B <= 10) (C >= 0 and C <= 10)):
+        print(media=(A * 0.2)+(B * 0.3)+(C * 0.5));
+    else:
+        print('tenet de novo');
